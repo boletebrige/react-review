0b878e5e301b27750c7feb2ac563f1062393f18c
boletebrige <josip.ravas.broj@gmail.com>
2017-09-12 14:21:27 +0200

    first react component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,5 @@
-console.log('radi');
+import React from 'react';
+import { render } from 'react-dom';
+import { App } from './components/App.js';
+
+render(<App />, window.document.getElementById('app'));
